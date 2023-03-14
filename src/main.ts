4b0a9972f16a7c9e611dbe8323893e165d20ffb8
Elad <eladdevmail@gmail.com>
2023-03-14 15:55:11 +0200

    minor change

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-/* import the fontawesome */
+/* import fontawesome */
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faUserSecret, faCartShopping, faTrash } from "@fortawesome/free-solid-svg-icons";
