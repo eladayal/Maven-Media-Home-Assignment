6af14f78fb43c9e5746d5d16e79f3bc3b09e0114
Elad <eladdevmail@gmail.com>
2023-03-14 17:25:18 +0200

    editing configfile path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/Maven-Media-Home-Assignment/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Maven-Media-Home-Assignment/" : "/",
   transpileDependencies: true,
 });
